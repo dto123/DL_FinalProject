62a4cf6e16146919abadc65f67a0ca1636ed7bca
David Ter-Ovanesyan <dterovanesyan@gmail.com>
2018-12-08 14:34:23 -0500

    Add files via upload

---
+++ initial_autoencoder.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 16:31:06 2018
+
+@author: eric
+"""
+
+import random
+import numpy as np
+#from cs682.data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
+import scipy.io as sio
+import matplotlib.image as image
+from sklearn.metrics import mean_squared_error
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import time
+import torch.utils.data as Data
+
+from load_images import load_images
+
+
+height, width = 10,10
+
+X_train = torch.from_numpy(load_images(100,height,width)).float()
+
+
+train_loader = Data.DataLoader(dataset = X_train, batch_size = 64, shuffle = True)
+###################################################################
+
+learning_rate = 1e-5
+
+in_size = height*width
+reduction=1
+
+model = nn.Sequential(
+    nn.Linear(in_size,int(in_size/reduction)),
+    nn.ReLU(True),
+    # coded is here
+    nn.Linear(int(in_size/reduction), in_size)
+)
+
+# define optimizer
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+accuracies = []
+epochs=1000
+loss_fn = nn.MSELoss()
+
+for e in range(epochs):
+
+    for step, x in enumerate(train_loader):
+        #model.train()  # put model to training mode
+        
+        #batch = np.random.choice(X.shape[0], 50)
+        
+        batch_x  = x.view(-1, in_size)
+        
+        
+        #reconstructed = model(X[batch])
+        reconstructed = model(batch_x)
+        
+        loss = loss_fn(reconstructed, batch_x)
+        #print(loss)
+        print('epoch: ', e, 'step: ', step, 'loss: ', loss)   
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        
+        
+   # print('epoch: ', e, 'step: ', step, 'loss: ', loss)        
+def check_image(model, X, ind, height, width):
+    
+    reconstructed = model(X)
+    img = reconstructed.detach().numpy()[ind].reshape(height, width,3)
+    orig_img = X.detach().numpy()[ind].reshape(height, width,3)
+    plt.imshow(img)
+    #plt2.imshow(orig_img)    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ load_images.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 17:02:11 2018
+
+@author: eric
+"""
+
+from PIL import Image
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def load_images(max_num_load, h_dim, w_dim):
+    imgs = []
+    for filename in os.listdir('ImageNet'):
+        if filename.endswith(".JPEG"): 
+            
+            # load image
+            jpgfile = Image.open('ImageNet/'+filename)
+            s = np.array(jpgfile)
+            if(s.shape[0] < h_dim or s.shape[1] < w_dim or s.ndim!=3): continue
+            
+            # crop image
+            s = s[:h_dim, :w_dim, :]
+            
+            imgs.append(s)
+            if len(imgs) >= max_num_load: break
+            if len(imgs) % 10 == 0: print(len(imgs))
+        
+    return np.array(imgs)
+
+imgs=load_images(100,100,100)
+
+print(imgs.shape)
