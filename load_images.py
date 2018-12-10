e04ab7903b2973c92a3bd993ac741befc5dbaff2
David T <dterovanesyan@gmail.com>
2018-12-10 16:44:45 -0500

    new size

--- load_images.py
+++ load_images.py
@@ -15,32 +15,38 @@ import numpy as np
 def load_images(max_num_load, h_dim, w_dim, bw=False):
     imgs = []
     for filename in os.listdir('ImageNet'):
-        if filename.endswith(".JPEG"): 
-            
+        if filename.endswith(".JPEG"):
+
             if(bw):
                 jpgfile = Image.open('ImageNet/'+filename)
                 s = np.array(jpgfile)
                 if(s.shape[0] < h_dim or s.shape[1] < w_dim or s.ndim!=2):continue
                 # crop image
                 s = s[:h_dim, :w_dim]
-                
+
                 imgs.append(s)
                 if len(imgs) >= max_num_load: break
                 if len(imgs) % 10 == 0: print(len(imgs))
                 continue
-            
+
             # load image
             jpgfile = Image.open('ImageNet/'+filename)
             s = np.array(jpgfile)
             if(s.shape[0] < h_dim or s.shape[1] < w_dim or s.ndim!=3): continue
-            
+
+            print(s.shape)
+            print(h_dim)
+            print(w_dim)
+
             # crop image
             s = s[:h_dim, :w_dim, :]
-            
+
+            print(s.shape)
+
             imgs.append(s)
             if len(imgs) >= max_num_load: break
             if len(imgs) % 10 == 0: print(len(imgs))
-        
+
     return np.array(imgs)
 
 #imgs=load_images(1000,500,500)
